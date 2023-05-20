mapping a range of floats e.g. [-1, 1] to integers: also, keeping past functionality with only lower bound and precision lvl tested code with relative paths, reordered statements found bug with precision when converting to ints, additional half a step (0.00015 loss vs max 0.0001 expected)

main.py
@@ -20,32 +20,43 @@ ymret.index.is_monotonic_increasing  # True
 xcret.index.is_monotonic_increasing  # False, but will be sorted after unstack
 xret = xcret.unstack('companyid') # n x m arrays where n = number of dates, m = number of companies
 
-
 assert xret.index.is_monotonic_increasing
 
 xret = xret.asfreq(freq='B')  # change datetime freq to business days
-
+xret.index
 
 assert all(xret.index == ymret.index)
 
+arr = np.asfortranarray(xret)  # columnar operation in theory faster on Fortran arrays,
+
+fname = Path(os.getcwd())
+persist_rets_bin(str(fname) + r'\floatGzipped', xret)  # 155 MB
 
 # quick search of compressors suggested to use blosc2
 import blosc2 as bl2
-blarr = bl2.compress2(np.ascontiguousarray(arr))
 
-savedsz = bl2.save_array(blarr, str(Path(fname, 'gfdgdf.bl2')), mode='w')
-# worse than npz
+compressed = bl2.pack_array(np.ascontiguousarray(xret))  # incl. clvl9 and shuffle
+
+# Save the compressed data to a file, > 200 MB
+with open('cr_compressed_blosc.b2frame', 'wb') as f:
+    f.write(compressed)
+
+blarr = bl2.compress2(np.ascontiguousarray(arr))  # fails with Fortran array
+# savedsz = bl2.save_array(blarr, str(Path(fname, 'cr_compressed_blosc.bl2')), mode='w')
+# this works but floods console, anyway worse than npz 162 MB
 
 import h5py
-with h5py.File(str(Path(fname, 'cr_compressed9.h5')), "w") as f:
+with h5py.File(str(Path(fname, 'crDF_compressed9.h5')), "w") as f:
     # same size
-    # dset_coefs = f.create_dataset("a", data=arr, compression="gzip", compression_opts=9)
-    dset_coefs = f.create_dataset("a", data=arr, compression="gzip", compression_opts=9, shuffle=True)
-    # dset_coefs = f.create_dataset("a", data=arr, compression="SZIP", compression_opts=9)
-    # dset_coefs = f.create_dataset("a", data=arr, compression="ZFP", compression_opts=9)
-    # dset_coefs = f.create_dataset("a", data=arr, compression="LZF", compression_opts=9)
-    # dset_coefs = f.create_dataset("a", data=arr, compression="LZMA", compression_opts=9)
-
+    # dset_coefs = f.create_dataset("a", data=xret, compression="gzip", compression_opts=9)
+    dset_coefs = f.create_dataset("a", data=xret, compression="gzip", compression_opts=9, shuffle=True)
+    # dset_coefs = f.create_dataset("a", data=xret, compression="SZIP", compression_opts=9)
+    # dset_coefs = f.create_dataset("a", data=xret, compression="ZFP", compression_opts=9)
+    # dset_coefs = f.create_dataset("a", data=xret, compression="LZF", compression_opts=9)
+    # dset_coefs = f.create_dataset("a", data=xret, compression="LZMA", compression_opts=9)
+# enabling shuffling can potentially improve the compression ratio without modifying the original data
+#  after decompression will be the same as the original data. 177 MB, rather slow
+#  xret or xret.values, no difference
 
 import pyarrow as pa
 import pyarrow.parquet as pq
@@ -54,8 +65,8 @@ import pyarrow.parquet as pq
 table = pa.Table.from_pandas(xret)
 
 # Write PyArrow Table to Parquet file
-pq.write_table(table, 'array.parquet')  # already less than pkl only 20% more than np compressed
-pq.write_table(table, 'xretgzipped.parquet', compression='gzip')  # already less than pkl only 20% more than np compressed
+pq.write_table(table, 'xret.parquet')  # already less than pkl only 20% more than np compressed
+pq.write_table(table, 'xretgzipped.parquet', compression='gzip')
 # gzip only 3% improvement over uncompressed parquet, zstd same, no improvement with brotli either
 
 
@@ -71,38 +82,36 @@ np.nanmin(arr)
 
 
 # numpy.uint16  # 0 to 65535, whereas uint8 is 0 to 255 (not enough for either company ids or dates)
-
+np.iinfo(np.uint16).max
 
 # 32-bit floating-point values cover range from 1.175494351 * 10^-38 to 3.40282347 * 10^+38,
 # whcih is too much for stock returns, so we may want to limit precision to 4 or 5 digits after the decimal point
 # (e.g. 1.2345e-2) and use integers to count number of steps (e.g. 0.0001 step size for 4 digits after the decimal point)
 # from minimal value, which for stock returns is -1, so we can use uint16 for that:
+np.nanmin(arr[arr>0])
+np.nanmax(arr[arr<0])
+np.nanmax(np.abs(arr))
+np.nanmin(np.abs(arr))
 
 # first, use defined function to convert float64 to uint16 per this logic:
 
+float_to_uint_minret(xret.values, prec=5, minval=-0.1)  # can use for our random data
+float_to_uint_minret(xret.values, prec=4, minval=-1)  # appropriate defaults for stock returns
 
-a = float_to_uint_minret(xret.values, prec=2)
-a = float_to_uint_minret(xret.values, prec=5, minval=-0.1)  # can use for our random data
-a = float_to_uint_minret(xret.values, prec=4, minval=-1)  # appropriate defaults for stock returns
 
-np.unique(a)
-a[a>0].min()
-a.max()
+# calstart = dt.datetime(2000, 1, 1)
 
-arr= xret
-calstart = dt.datetime(2000, 1, 1)
-
-fname_ = Path(os.getcwd())
-
-persist_rets_bin(str(fname_) + r'\fl', xret)
-persist_rets_bin(str(fname_) + r'\uint16off', xret, to_int=np.uint16)
+persist_rets_bin(str(fname) + r'\uint16offst', xret, to_int=np.uint16)
 # 80% reduction in size, but obviously we lose precision, may be outside of the task scope
 
-
-xret2 = load_rets_from_bin('uint16off_npcomprsd.npz', val_dtype=np.uint16, prec=4, minval=-1)
+xret2 = load_rets_from_bin('uint16offst_npcomprsd.npz', val_dtype=np.uint16, prec=4, minval=-1)  # also converts uint16 to float64
 
 prec = 4
 stepsz = 10**(-prec)  # fails with smaller rounding or np.close 0
 assert np.nanmax(np.abs(xret.values.round(4) - xret2.values)) < stepsz, "reconstructed values differ from original"
-# may still some bug in my code, which minimally distorts precision, needs improvement before production use
+assert np.nanmax(np.abs(xret.values - xret2.values)) < stepsz*2, "reconstructed values differ from original"
+# may still some bug in my code, which minimally distorts precision up to 0.0002 not 0.0001
+#  needs improvement before production use
+np.nanmean(np.abs(xret.values - xret2.values))
+:q
 