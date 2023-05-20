added unit test setup with pytest, ran multiple times successfully fixed bug with precision (see last commit), refactoring re-ran main file, ok

main.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 import datetime as dt
 
-from p1_funs import *
+from p1_funs import persist_rets_bin, load_rets_from_bin, floatr_to_uint_minret, uint_ret_offs_tofloatr
 
 xcret = pd.read_pickle('data/company_returns.pkl')
 ymret = pd.read_pickle('data/market_returns.pkl')  # bdays only
@@ -95,13 +95,13 @@ np.nanmin(np.abs(arr))
 
 # first, use defined function to convert float64 to uint16 per this logic:
 
-float_to_uint_minret(xret.values, prec=5, minval=-0.1)  # can use for our random data
-float_to_uint_minret(xret.values, prec=4, minval=-1)  # appropriate defaults for stock returns
+floatr_to_uint_minret(xret.values, prec=5, minval=-0.1)  # can use for our random data
+floatr_to_uint_minret(xret.values, prec=4, minval=-1)  # appropriate defaults for stock returns
 
 
 # calstart = dt.datetime(2000, 1, 1)
 
-persist_rets_bin(str(fname) + r'\uint16offst', xret, to_int=np.uint16)
+persist_rets_bin(str(fname) + r'\uint16offst', xret, to_int=np.uint16, prec=4, minval=-1)  # 34 MB
 # 80% reduction in size, but obviously we lose precision, may be outside of the task scope
 
 xret2 = load_rets_from_bin('uint16offst_npcomprsd.npz', val_dtype=np.uint16, prec=4, minval=-1)  # also converts uint16 to float64
@@ -109,9 +109,26 @@ xret2 = load_rets_from_bin('uint16offst_npcomprsd.npz', val_dtype=np.uint16, pre
 prec = 4
 stepsz = 10**(-prec)  # fails with smaller rounding or np.close 0
 assert np.nanmax(np.abs(xret.values.round(4) - xret2.values)) < stepsz, "reconstructed values differ from original"
-assert np.nanmax(np.abs(xret.values - xret2.values)) < stepsz*2, "reconstructed values differ from original"
-# may still some bug in my code, which minimally distorts precision up to 0.0002 not 0.0001
-#  needs improvement before production use
+assert np.nanmax(np.abs(xret.values - xret2.values)) < stepsz, "reconstructed values differ from original"
+#  may need improvement before production use
 np.nanmean(np.abs(xret.values - xret2.values))
-:q
+
+
+
+# also, test range functionality, mapping e.g. floats betweer values [-1, 1] to ints [0, 65535] assumption of fixed step
+xret3rng = floatr_to_uint_minret(xret.values.copy(), minval=-0.07, maxval=0.07)
+xret3_orng = uint_ret_offs_tofloatr(xret3rng, minval=-0.07, maxval=0.07)
+np.nanmean(np.abs(xret.values.round(4) - xret3_orng))  # strangely on range precision is worse than on minret only, same with nanmax
+
+
+
+xret3rngu = floatr_to_uint_minret(xret.values.copy(), prec=4, minval=-1)
+xret3_orngu = uint_ret_offs_tofloatr(xret3rngu, prec=4, minval=-1)
+
+np.nanmean(np.abs(xret.values.round(4) - xret3_orngu))  # indeed, yields smallest error, same with nanmax
+
+# chk worst case max abs error
+np.nanmax(np.abs(xret.values.round(4) - xret3_orngu))  # 0.0001
+np.nanmax(np.abs(xret.values.round(4) - xret3_orng))  # 0.0001
+
 

p1_funs.py
@@ -2,31 +2,79 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 
-def float_to_uint_minret(arr: np.ndarray, prec: int = 4, dtype_: np.dtype = np.uint16, minval: float = -1)\
-        -> np.ndarray:
+# todo: extend both functions to allow for upper bound (maxval) only to be specified,
+#       implement e.g. as a recursive function that calls itself with minval = -maxval
+def floatr_to_uint_minret(arr: np.ndarray, dtype_: np.dtype = np.uint16, prec: int = None,
+                          minval: float = -1, maxval: float = None) -> np.ndarray:
     """
-    Convert float64 array to integers of specified type using given levels of minval and precision.
+    Convert float64 array to integers of specified type using given levels of minval and precision or
+     range from minval to maxval (precision would then depend on dtype_).
 
-    To save space by rounding and storing as integers e.g. stock returns represented as offsets
+    To save space by either a) rounding and storing as integers e.g. stock returns represented as offsets
      (of steps sized according to level of precision) from minimum possible return (e.g. -1 or -100%).
      E.g. uint16 array with minimum value of minval and precision of prec digits after the decimal point.
+     or b) by mapping a range of floats e.g. [-1, 1] to integers, using equal spacing e.g. 255 for uint8.
 
     :param arr: array of float64 values representing stock returns
-    :param prec: number of digits after the decimal point to keep
     :param dtype_: integer data type to use for storing the result (should be unsigned and have enough bits for prec)
-    :param minval: minimum possible value for stock return (e.g. -1 or -100%), used to calculate offset
+    :param prec: number of digits after the decimal point to keep (requires minval to be specified but not maxval)
+    :param minval: minimum possible value for stock return (e.g. -1 or -100%), used to calculate offset (a) or see maxval
+    :param maxval: for b) maximum possible value for stock return (e.g. 1 or 100%), changes mapping of floats to integers
+        using a range of integers from 0 to maxval-minval (e.g. 0 to 2 for minval=-1 and maxval=1),
+        cannot be used together with prec! to-do:  allow for upper bound (maxval) with prec and no minval,
+        implement similar to current minval e.g. as a recursive function that calls itself with minval = -maxval
     :return:
     """
     assert arr.ndim == 2
     assert arr.dtype == np.float64
-    stepsz = 10**(-prec)  # step size for given precision; reserve 0 for nan, (first +stepsz) for actual minval:
-    rarr = arr + (-minval + stepsz) # can also use arr.min(), but solving for general case (not random data)
-    assert np.nanmin(rarr) > 0, f"unexpected value for stock return < minval ({minval})"
-    # rarr = rarr.round(prec) / stepsz  # alternatively, we can use rarr // stepsz
-    rarr = rarr.round(prec) * 10**prec  # alternatively, we can use rarr // stepsz
-    assert np.nanmax(rarr) <= np.iinfo(dtype_).max, f"overflown {dtype_} with > {np.iinfo(dtype_).max}"
-    rarr = rarr.astype(dtype_) # reserve 0 for nan. 1 is -1 (lowest possible value for stock return)
-    return rarr
+    rarr = arr + (-minval)
+    assert np.nanmin(rarr) >= 0, f"unexpected value for stock return < minval ({minval})"
+    if maxval and prec is None:
+        assert minval < maxval
+        stepsz = (maxval - minval) / (np.iinfo(dtype_).max - 1)  # -1 is to reserve 0 for NaNs
+        rarr /= stepsz
+    else:
+        assert prec and maxval is None, "either maxval or prec must be specified and not both"
+        stepsz_rcp = 10 ** prec
+        rarr *= stepsz_rcp
+    np.round(rarr, out=rarr)
+    assert np.nanmax(rarr) < np.iinfo(dtype_).max, f"overflown {dtype_} with > {np.iinfo(dtype_).max}"
+    return rarr.astype(dtype_) + 1  # reserve 0 for nans
+
+def uint_ret_offs_tofloatr(arr: np.ndarray, dtype_: np.dtype = np.uint16, prec: int = None,
+                           minval: float = -1, maxval: float = None) -> np.ndarray:
+    """
+    Convert integer array to float64 array using given levels of minval and precision, or maxval and minval only.
+
+    To restore float64 array from integers representing stock returns as either offsets
+     (of steps, sized according to level of precision) from minimum possible return (e.g. -1 or -100%), or
+     or equal steps within range of minval and maxval.
+     E.g. uint16 array with minimum value of minval, precision of prec digits after the decimal point, and
+     unspecified maxval (limited by dtype_) or uint16 array with minimum value of minval, unspecified precision,
+     maximum value of maxval.
+
+    :param arr: array of integers representing stock returns
+    :param dtype_: integer data type used for storing the result
+    :param prec: number of digits after the decimal point that was kept [to-do: save this as metadata to auto-reconstruct]
+    :param minval: minimum possible value for stock return (e.g. -1 or -100%), which was used to calculate offset or see maxval
+    :param maxval: maximum possible value for stock return (e.g. 1 or 100%) used, changes mapping of floats to integers
+        using a range of integers from 0 to maxval-minval (e.g. 0 to 2 for minval=-1 and maxval=1),
+        cannot be used together with prec! to-do:  allow for upper bound (maxval) with prec and no minval,
+        implement similar to current minval e.g. as a recursive function that calls itself with minval = -maxval
+    :return: array of float64 values representing stock returns
+    """
+    assert arr.ndim == 2
+    assert arr.dtype == dtype_
+    out = np.full_like(arr, np.nan, dtype=np.float64)
+    if maxval and prec is None:
+        assert minval < maxval
+        stepsz = (maxval - minval) / (np.iinfo(dtype_).max - 1)
+        out[arr != 0] = (arr[arr != 0] - 1) * stepsz + minval  # subtract 1 as 0 was reserved for nans, 1 for minval
+    else:
+        assert prec and maxval is None, "either maxval or prec must be specified and not both"
+        stepsz_rcp = 10 ** prec  # reciprocal
+        out[arr != 0] = (arr[arr != 0] - 1) / stepsz_rcp + minval  # subtract 1 as 0 was reserved for nans, 1 for minval
+    return out
 
 def persist_rets_bin(fname: str, arr: np.ndarray | pd.DataFrame,
                      calstart: np.datetime64 = dt.datetime(2000, 1, 1),
@@ -63,31 +111,9 @@ def persist_rets_bin(fname: str, arr: np.ndarray | pd.DataFrame,
     arr = np.asfortranarray(arr)
     # assert arr.dtype == np.float64  # now also using to save example offset int values:
     if to_int:
-        arr = float_to_uint_minret(arr, dtype_=to_int, **kwargs)
+        arr = floatr_to_uint_minret(arr, dtype_=to_int, **kwargs)
     np.savez_compressed(str(fname) + '_npcomprsd', arr=arr, firms=firms, bd_offs=bd_offs)
-
-
-
-def uint_ret_offs_tofloat(arr: np.ndarray, prec=4, minval=-1, dtype_: np.dtype = np.uint16) -> np.ndarray:
-    """
-    Convert integer array to float64 array using given levels of minval and precision.
-
-    To restore float64 array from integers representing stock returns as offsets
-     (of steps, sized according to level of precision) from minimum possible return (e.g. -1 or -100%).
-     E.g. uint16 array with minimum value of minval and precision of prec digits after the decimal point.
-
-    :param arr: array of integers representing stock returns
-    :param prec: number of digits after the decimal point that was kept
-    :param dtype_: integer data type used for storing the result
-    :param minval: minimum possible value for stock return (e.g. -1 or -100%), which was used to calculate offset
-    :return: array of float64 values representing stock returns
-    """
-    assert arr.ndim == 2
-    assert arr.dtype == dtype_
-    out = np.full_like(arr, np.nan, dtype=np.float64)
-    stepsz = 10**(-prec)
-    out[arr != 0] = arr[arr != 0] * stepsz - (-minval + stepsz)
-    return out
+    return None
 
 
 def load_rets_from_bin(fpath: str, as_numpy: bool = False,
@@ -117,7 +143,7 @@ def load_rets_from_bin(fpath: str, as_numpy: bool = False,
         val_dtype = np.float64
         assert arr.dtype == val_dtype
     else:
-        arr = uint_ret_offs_tofloat(arr, prec=prec, minval=minval, dtype_=val_dtype)
+        arr = uint_ret_offs_tofloatr(arr, prec=prec, minval=minval, dtype_=val_dtype)
     if as_numpy:
         return arr
     else: