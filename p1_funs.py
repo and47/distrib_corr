added tests for conversion and overflow, refactoring use of pytest testing using fixed and random data, added parametrization fixture fixed a bug in my float to uint conversion, found by proper test np.allclose

p1_funs.py
@@ -32,14 +32,16 @@ def floatr_to_uint_minret(arr: np.ndarray, dtype_: np.dtype = np.uint16, prec: i
     if maxval and prec is None:
         assert minval < maxval
         stepsz = (maxval - minval) / (np.iinfo(dtype_).max - 1)  # -1 is to reserve 0 for NaNs
+        rarr += stepsz
         rarr /= stepsz
     else:
         assert prec and maxval is None, "either maxval or prec must be specified and not both"
         stepsz_rcp = 10 ** prec
+        rarr += 1 / stepsz_rcp  # increment by 1 step to reserve 0 for NaNs, stepsz for 0 (minreturn=-1)
         rarr *= stepsz_rcp
     np.round(rarr, out=rarr)
     assert np.nanmax(rarr) < np.iinfo(dtype_).max, f"overflown {dtype_} with > {np.iinfo(dtype_).max}"
-    return rarr.astype(dtype_) + 1  # reserve 0 for nans
+    return rarr.astype(dtype_)
 
 def uint_ret_offs_tofloatr(arr: np.ndarray, dtype_: np.dtype = np.uint16, prec: int = None,
                            minval: float = -1, maxval: float = None) -> np.ndarray:

tests/test_p1funsOnMainData.py
@@ -2,7 +2,11 @@ import numpy as np
 import pandas as pd
 
 import pytest
+import tempfile
+
 #import main
+from p1_funs import persist_rets_bin, load_rets_from_bin, floatr_to_uint_minret, uint_ret_offs_tofloatr
+
 
 # initiate one fixed fixture with preset seed (for reproducible results, benchmarking)
 # and one with random values (for testing universal application of functions)
@@ -14,6 +18,8 @@ seed = 123
 genor_ran = np.random.default_rng()  # new per NEP 19, can be used with Cython
 genor_fix = np.random.default_rng(seed)
 
+# unfortunately, benchmark plugin only tests speed, not memory (disk usage); so not for compression comparison
+#  so this wasn't included in tests also in the interest of time
 
 def create_test_data(genor, N_companies = 5000, N_dates = 4000, dates = pd.bdate_range('2000-01-01', pd.Timestamp.today())):
     index = [list(zip(repeat(i), genor.choice(dates, size=N_dates, replace=False))) for i in range(N_companies)]
@@ -52,14 +58,32 @@ def test_initiate(fixed_data, rndm_data):
     print('random data (mkt and stock mins): ', np.nanmin(rndm_data[0]), np.nanmin(dfran))  # should be different
     print(' fixed data (mkt and stock mins): ', np.nanmin(market), np.nanmin(df))  # should be:
     # -0.028345506444168257 -0.06909354203029808
-#
-# test_overflow
-#
-# test_coversion
+
+@pytest.mark.parametrize('data', ['fixed_data', 'rndm_data'])
+def test_conversion(request, data, benchmark):
+    _, df = request.getfixturevalue(data)  # market not used
+    crets = df.unstack('companyid')
+    assert crets.index.is_monotonic_increasing
+    crets = crets.to_numpy()
+    crets_rc = floatr_to_uint_minret(crets, prec=4, minval=-1)  # rounded and converted
+    assert crets_rc.shape == crets.shape
+    assert crets_rc.dtype == np.uint16
+    crets_re = uint_ret_offs_tofloatr(crets_rc, prec=4, minval=-1)  # restored/reconstructed from uint16 object
+    assert np.nanmin(crets_re) >= -1
+    assert crets_re.shape == crets.shape
+    assert crets_re.dtype == crets.dtype
+    assert np.nanmax(np.abs(crets_re - crets)) < 10**(-4), "reconstructed values differ from original"
+    assert np.allclose(np.nan_to_num(crets_re), np.nan_to_num(crets), atol=1e-4), "reconstructed values differ from original"  # 1e fails
+
+@pytest.mark.parametrize('data', ['fixed_data', 'rndm_data'])
+def test_overflow(request, data):
+    _, df = request.getfixturevalue(data)  # market not used
+    with pytest.raises(AssertionError):  # to do: use Exceptions in actual code, leave asserts for testing (test_* files)
+        crets_re = floatr_to_uint_minret(df.values, prec=6, minval=-1)
+
 #
 # test_reconstruction
 #
 # test_savedsize
 
 
-# unfortunately, benchmark plugin only tests speed, not memory (disk usage); so not for compression comparison